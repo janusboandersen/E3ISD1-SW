0212c70b61e9355c79d6e91cbdab1d1c3cdde86b
Janus Bo Andersen <janus@janusboandersen.dk>
2019-09-16 12:52:34 +0800

    New project files

---
+++ Experiments/GottschlingC++/Ch1_01/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "The answer to the Ultimate question of Life,\n"
+              << "the Universe, and Everything is:"
+              << std::endl << 6 * 7 << std::endl;
+    return 0;
+}
