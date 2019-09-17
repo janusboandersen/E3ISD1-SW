dec7ec169949ce8b24fd78bcbe46ced828a7b8da
Janus Bo Andersen <janus@janusboandersen.dk>
2019-09-17 14:36:25 +0800

    done with ch 1.7

--- Experiments/GottschlingC++/Ch1_06/main.cpp
+++ Experiments/GottschlingC++/Ch1_06/main.cpp
@@ -3,8 +3,22 @@
  * */
 
 #include <iostream>
+#include <cstdlib>
+#include <exception>
+
+
+void test_func(void) {
+    throw std::runtime_error("Ooops!");
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    try {
+        test_func();
+    } catch (...) { //catch-all      std::exception& e
+        std::cerr << "Something went weirdly wrong... " << std::endl;  //e.what() <<
+    }
+
+    return EXIT_SUCCESS;
 }
