c4f5030cc5d03b62d299d56581181c5807f04769
Janus Bo Andersen <janus@janusboandersen.dk>
2019-09-17 12:01:33 +0800

    Add chapter 1.6 exception handling.

---
+++ Experiments/GottschlingC++/Ch1_06/main.cpp
@@ -0,0 +1,10 @@
+/* Test functionality from ch. 1.6 exception handling
+ * Janus Bo Andersen, Sep 2019
+ * */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
