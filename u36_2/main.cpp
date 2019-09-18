4d2d0671f1cb405c93817d87aad88258a982bc56
Janus Bo Andersen <janus@janusboandersen.dk>
2019-09-18 18:35:20 +0800

    Start week 36/2

---
+++ u36_2/main.cpp
@@ -0,0 +1,11 @@
+/* E3ISD1 Week 36/2 -- Exercise 4
+ *
+ * Janus Bo Andersen, Sep 2019
+ * */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
